c30a7aa5d0fd48df1e4ec089f455c2da23c6bdce
lnemec <lydia.nemec@gmail.com>
2022-08-21 08:56:47 +0200

    First introduction in experiment folder, to generate the data.

---
+++ eperiments/0_intro.jl
@@ -0,0 +1,10 @@
+import VonNeumannElephant as VNE
+
+println("")
+println("!!!Generate and plot the von Neumann elephant!!!")
+println("")
+
+# Number of points
+N_full = 1000
+
+elephant_full = VNE.get_elephant(N_full, -pi, pi)
